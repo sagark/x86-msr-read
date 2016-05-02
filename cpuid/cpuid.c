d27a2fcd39deb8afa8f63af3790b8114424e5e74
Sagar Karandikar <sagark@eecs.berkeley.edu>
2016-05-02 10:27:50 -0700

    cpuid reader

---
+++ cpuid/cpuid.c
@@ -0,0 +1,29 @@
+#include <cpuid.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    unsigned int eax, ebx, ecx, edx, eax_in, ecx_in;
+    // see for info:
+    // https://en.wikipedia.org/wiki/CPUID#EAX.3D0:_Highest_Function_Parameter
+
+    // eax_in and eax are different because of 
+    // input operand handling. see:
+    // 6.44.2.5 Input Operands
+    // in: https://gcc.gnu.org/onlinedocs/gcc/Extended-Asm.html
+
+    eax_in = 0xa;
+    ecx_in = 0;
+
+    eax = 0;
+    ecx = 0;
+    ebx = 0;
+    edx = 0;
+
+    __cpuid_count(eax_in, ecx_in, eax, ebx, ecx, edx);
+
+    printf("eax: %x\n", eax);
+    printf("ebx: %x\n", ebx);
+    printf("ecx: %x\n", ecx);
+    printf("edx: %x\n", edx);
+}
